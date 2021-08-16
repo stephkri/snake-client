460450a10e3cbf87c6068c92b24467ac2e4d49d2
stephkri <stephane.krims@hotmail.ca>
2021-08-16 22:42:58 +0000

    Added handleUserInput and called it in setupInput

--- play.js
+++ play.js
@@ -1,5 +1,29 @@
 const connect = require("./client.js");
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+const handleUserInput = function(stdin) {
+  stdin.on("data", (key) => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  handleUserInput(stdin);
+  return stdin;
+};
+
+setupInput();
+
 // Establish connection w game server
 
 console.log("Connecting...");
